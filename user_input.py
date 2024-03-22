14bb1313dd5396f737604f177bfa808e3dd9808d
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-22 13:46:37 +0300

    week 2 assignment

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+message = f"Hello {name}, you are {age} years old and live in {location}."
+print(message)
